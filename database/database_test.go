460ca59565820604e7d219737b760dfac22b5a29
MiroslavZaprazny <miro.zaprazny8@gmail.com>
2023-09-11 11:56:34 -0700

    more tests

--- database/database_test.go
+++ database/database_test.go
@@ -22,3 +22,24 @@ func TestGetDriverNameFromSource(t *testing.T) {
         }
     }
 }
+
+func TestGetUrlFromSource(t *testing.T) {
+    tests := []struct {
+        input string
+        expected string
+    } {
+        {"mysql://root:root@localhost:3306/test", "root:root@tcp(localhost:3306)/test"},
+        {"postgre://testUser:password@127.0.0.1:3306/test", "testUser:password@tcp(127.0.0.1:3306)/test"},
+    }
+
+    for _, test := range tests {
+        url, err := getUrlFromSource(test.input)
+        if err != nil {
+            t.Errorf("Got error %s", err.Error())
+        }
+
+        if url != test.expected {
+            t.Errorf("Expcted %s got %s", test.expected, url)
+        }
+    }
+}
--- migration/2023-09-09-12-32-37-test_up.sql
+++
@@ -1,1 +0,0 @@
-CREATE table testing(test TEXT);
